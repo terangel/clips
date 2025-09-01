18f607b7ce1b4db7593528f335248fa1c560f357
angel.teran <angel.teran@elasthink.com>
2025-09-01 09:29:09 +0200

    Changes.

---
+++ demo/clips/today/handler.js
@@ -0,0 +1,13 @@
+import clips from '../../../clips.js';
+
+clips.define('today', {
+
+    render: function(options) {
+        return /*html*/`
+            <div class="today-clip clip">
+                TODAY
+            </div>
+        `;
+    }
+    
+});
