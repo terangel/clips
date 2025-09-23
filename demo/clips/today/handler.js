a72207beb3dc2e403353310e8399ec105f4c45a0
angel.teran <angel.teran@elasthink.com>
2025-09-23 21:24:17 +0200

    Changes.

--- demo/clips/today/handler.js
+++ demo/clips/today/handler.js
@@ -8,6 +8,22 @@ clips.define('today', {
                 TODAY
             </div>
         `;
+    },
+
+    load: async function(options) {
+        const res = await fetch('sample-data/today.json');
+        if (res.ok) {
+            return res.json();
+        }
+    },
+
+    update: function(options) {
+        // Vaciamos el contenido del elemento raíz.
+        this.root.replaceChildren();
+        // Generamos las fichas.
+        for (const item of options.data) {
+            this.root.innerHTML += `<div class="today-item">${item.description}</div>`;
+        }
     }
     
 });
